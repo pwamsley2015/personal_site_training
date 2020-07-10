44264cfe941c349b16aace2696e6b8aded65f0e8
Patrick Wamsley <pwamsley2015@francisparker.org>
2020-07-09 21:33:17 -0700

    Parallax background images

--- src/index.js
+++ src/index.js
@@ -2,27 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Parallax from "react-springy-parallax";
 import "./index.css";
+import PreBenchImg from "./images/PreBench.png";
+
+const HELLO = "Hi, I'm Patrick";
+const BTW = "I'm a nerdy Powerlifter";
+const ABOUT =
+  "I started training seriously (began my first decent program) early 2018. I've made some progress and learned a few things since then...";
+const PRS = "Current PRS:";
+const SQUAT = "500";
+const BENCH = "286";
+const DEADLIFT = "600";
+const WHY = "This website exists because...";
 
 class HomeScreen extends React.Component {
   render() {
+    const styles = {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    };
     return (
       <div>
         <Parallax ref="parallax" pages={3}>
-          <Parallax.Layer offset={0} speed={0.5}>
-            Layer 0 Image
-          </Parallax.Layer>
-          <Parallax.Layer offset={0.5} speed={0.5}>
-            Layer 1 Hi I'm Patrick
-          </Parallax.Layer>
-          <Parallax.Layer offset={1} speed={0.5}>
-            Layer 2 Links
-          </Parallax.Layer>
-          <Parallax.Layer offset={1.5} speed={0.5}>
-            Layer 3 Coaching
-          </Parallax.Layer>
-          <Parallax.Layer offset={2} speed={0.5}>
-            Layer 4 Ted Talks
-          </Parallax.Layer>
+          {/* Backgrounds */}
+          <Parallax.Layer
+            offset={0}
+            speed={1}
+            style={{
+              backgroundImage: "url(" + PreBenchImg + ")",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          />
+          <Parallax.Layer
+            offset={1}
+            speed={1}
+            style={{ backgroundColor: "#243B4a" }}
+          ></Parallax.Layer>
+
+          <Parallax.Layer
+            offset={2}
+            speed={1}
+            style={{ backgroundColor: "#243BFF" }}
+          ></Parallax.Layer>
+          {/* Content */}
+          <Parallax.Layer offset={0.65} speed={0.5}></Parallax.Layer>
         </Parallax>
       </div>
     );
